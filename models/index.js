d75acb0a4719329e724e1fad9df182911cf35b8a
Todd Kridel <todd.kridel@gmail.com>
2023-02-10 14:24:56 -0700

    Updated the route and model and seed all-component index files

--- models/index.js
+++ models/index.js
@@ -1,20 +1,65 @@
-// import models
-const Product = require('./Product');
+
+
+// Import the data models of the application.
 const Category = require('./Category');
+const Product = require('./Product');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+//
+// "Categories" data/table relationships:
+//
 
-// Products belongToMany Tags (through ProductTag)
+// for the one-to-many relationship with the "Products" table
+Category.hasMany(Product, {  // Categories have many Products.
+  foreignKey: 'category_id'
+});
 
-// Tags belongToMany Products (through ProductTag)
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+//
+// "Products" data/table relationships:
+//
+
+// for the many-to-many relationship with the "Category" table
+Product.belongsTo(Category, {  // Products belongs to a Category.
+  foreignKey: 'category_id', 
+})
+
+// for the many-to-many relationship with the "Tag" table
+Product.belongsToMany(Tag, {  // Products belong to many Tags (through ProductTag).
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
+
+
+//
+// "Tag" data/table relationships:
+//
+
+// for the many-to-many relationship with the "Tag" table
+Tag.belongsToMany(Product, {  // Tags belong to many Products (through ProductTag).
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
+
+// Export the data model associations for usage in other areas of the application.
+module.exports = { Category, Product, Tag, ProductTag };
+
+
+// Associations
+//
+// You'll need to execute association methods on your Sequelize models to create 
+// the following relationships between them:
+//
+// Product belongs to Category, as a category can have multiple products but a 
+// product can only belong to one category.
+//
+// Product belongs to many Tag models. Using the ProductTag through model, allow 
+// products to have multiple tags and tags to have many products.
+//
+// Category has many Product models.
+//
+// Tag belongs to many Product models.
+
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,32 @@
+
+
+// Establish the import of the Router system components that allow for multiple URL route-definition files.
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// Implement/Activate the route API middleware systems.
 router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+router.use('/', (req, res) => {
+//router.use((req, res) => {
+res.send("<h1>A non-correct route URL was used.</h1>")
 });
 
+
+// Export the Router system code for usage in other areas of the application.
 module.exports = router;
+
+
+// Fill out the API routes to perform RESTful CRUD operations.
+// Fill out the un-finished routes in product-routes.js, tag-routes.js, and category-routes.js
+// to perform create, read, update, and delete operations using your Sequelize models.
+
+
+// possible "controllers\index.js" file:
+//
+// const router = require('express').Router();
+// const homeRoutes = require('./homeRoutes');
+// const apiRoutes = require('./api');
+// router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
+// module.exports = router;
+
